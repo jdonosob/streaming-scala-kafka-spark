9e4b1f77d59ce39ad187bee53db1046344c0034f
jdonosob <donosobaraonaj@gmail.com>
2025-05-12 15:49:27 -0400

    feat: add data generator for project configuration
    - Implemented a data generator to kafka topic

--- data-generator/Main.scala
+++ data-generator/Main.scala
@@ -1,6 +1,8 @@
 import scala.util.Random
 import java.util.{Properties,UUID}
 import org.apache.kafka.clients.producer.{KafkaProducer, ProducerRecord, ProducerConfig}
+import org.apache.kafka.common.serialization.StringSerializer
+import java.time.Instant
 
 
 object Main {
@@ -9,18 +11,38 @@ object Main {
         val brokers = "localhost:9092"
 
         val props = new Properties()
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, brokers)
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, classOf[StringSerializer].getName)
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, classOf[StringSerializer].getName)
 
+        val producer = new KafkaProducer[String, String](props)
 
+        println(s"Sending data to Kafka topic [$topic]... Press Ctrl+C to stop.")
 
+        while (true) {
+            val sensorId = s"sensor-${Random.nextInt(100)}"
+            val temperature = 15 + Random.nextGaussian() * 10
+            val timestamp = Instant.now().toString
 
-        val random = new Random()
-        val numRows = 1000000
-        val numCols = 10
-        val data = Array.fill(numRows, numCols)(random.nextDouble())
+            val json = 
+                s"""
+                |{
+                |  "sensorId": "$sensorId",
+                |  "temperature": ${"%.2f".format(temperature)},
+                |  "timestamp": "$timestamp"
+                |}
+                """.stripMargin.replaceAll("\n", "")
 
-        // Print the first 10 rows of the generated data
-        for (i <- 0 until 10) {
-            println(data(i).mkString(", "))
+            val record = new ProducerRecord[String, String](topic, sensorId, json)
+            producer.send(record, (metadata, exception) => {
+                if (exception != null) {
+                    println(s"Error sending message: ${exception.getMessage}")
+                } else {
+                    println(s"Sent message to topic ${metadata.topic()} partition ${metadata.partition()} offset ${metadata.offset()}")
+                }
+            })
+            Thread.sleep(1000) // Sleep for 1 second before sending the next message
+
         }
     }
 }
